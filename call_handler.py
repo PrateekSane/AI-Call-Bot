3359b3e8b398899b1ee84a27deeaf50ba456925e
Prateek Sane <ptk1917@gmail.com>
2024-10-14 17:42:40 -0700

    redid confrence call pattern. still not working

--- call_handler.py
+++ call_handler.py
@@ -4,7 +4,9 @@ import argparse
 import dotenv
 from speech_checker import is_human_speech
 from utils import setup_logging, setup_twilio
-
+import os
+import requests
+from datetime import datetime
 # Load environment variables from the correct path
 dotenv.load_dotenv('env/.env')
 
@@ -16,21 +18,17 @@ TARGET_NUMBER = '+19164729906'
 
 app = Flask(__name__)
 
-# This route handles the incoming call and streams the audio
 @app.route("/", methods=['GET', 'POST'])
 def voice():
     """Respond to incoming calls and start audio processing"""
     resp = VoiceResponse()
     logger.info("Starting call")
     
-    # Start the conference when the call begins
-    dial = Dial()
-    dial.conference('MyConference', start_conference_on_enter=True, end_conference_on_exit=False)
-    #gather.say("Will text when ready. Holding")
-    gather = get_voice_gather()
+    # Start with a <Dial> verb
+    conference_dial = get_conference_dial(TARGET_NUMBER)
 
-    resp.append(gather)
-    resp.append(dial)
+    resp.append(get_voice_gather())
+    resp.append(conference_dial)
 
     return str(resp)
 
@@ -42,7 +40,9 @@ def process_audio():
     if speech_result:
         logger.info("Processing audio" + speech_result)
         if is_human_speech(speech_result):
+            logger.info("Human speech detected" + str(response))
             response = complete_call(response)
+            logger.info("Human speech detected" + str(response))
         else:
             # Continue gathering input
             gather = get_voice_gather()
@@ -54,58 +54,40 @@ def process_audio():
 
     return str(response)
 
-@app.route("/join_conference", methods=['GET', 'POST'])
-def join_conference():
-    """TwiML to rejoin the conference when the user is called back"""
+@app.route("/merge_in_user", methods=['GET', 'POST'])
+def merge_in_user():
+    """TwiML to merge the user back into the conference"""
     response = VoiceResponse()
-
+    logger.info("Merging user back into the conference")
     # Rejoin the user to the existing conference
-    dial = Dial()
-    dial.conference('MyConference', start_conference_on_enter=True, end_conference_on_exit=False)
-    
+    dial = get_conference_dial(TARGET_NUMBER)
     response.append(dial)
+
+    response.pause(5)
+
+    response.say("Exiting the call. Goodbye!")
+    response.leave()
+
     return str(response)
 
 
 def complete_call(response):
     """Complete the call"""
     logger.info("Completing call")
-    response.say("Exiting the call. Goodbye!")
-    call_user_back(response)
-
-    user_picked_up = False 
-    if user_picked_up:
-        logger.info("Bot is leaving the conference")
-        response.leave()
+    call_instance = call_user_back()
 
     return str(response)
 
-def call_user_back(response):
+def call_user_back():
     """Call the user back"""
-    #response.dial()
-    twilio_client.calls.create(
-        to=TARGET_NUMBER,  # Replace with the user's phone number to call back
-        from_=TWILIO_NUMBER,  # Replace with your Twilio number
-        url=FLASK_ADDRESS + '/join_conference',  # URL to TwiML that rejoins the conference
+    call_instance = twilio_client.calls.create(
+        to=TARGET_NUMBER,
+        from_=TWILIO_NUMBER,
+        url=FLASK_ADDRESS + '/merge_in_user',
         method='POST'
     )
-    return str(response)
+    return call_instance
 
-@app.route("/wait", methods=['POST'])
-def make_customer_service_wait():
-    """Make the customer service wait"""
-    response = VoiceResponse()
-    response.say("Thank you for picking up. I am an assistant and will connect you shortly")
-    return str(response)
-
-
-def send_alert(message):
-    """Send a text alert"""
-    twilio_client.messages.create(
-        body=message,
-        from_=TWILIO_NUMBER,
-        to=TARGET_NUMBER
-    )
 
 def get_voice_gather():
     return Gather(input='speech', 
@@ -114,8 +96,62 @@ def get_voice_gather():
                   speech_timeout=7,
                   actionOnEmptyResult=True)
 
-def flush_logger():
-    logger.handlers.clear() 
+def get_conference_dial(from_number):
+    dial = Dial()
+    if from_number == TARGET_NUMBER:
+        dial.conference(
+            'InitialConference',
+            start_conference_on_enter=False,
+            end_conference_on_exit=False)
+    else:
+        # Otherwise have the caller join as a regular participant
+        dial.conference(
+            'FinalConference',
+            start_conference_on_enter=True,
+            end_conference_on_exit=False)
+
+    return dial 
+
+
+# @app.route("/wait", methods=['POST'])
+# def make_customer_service_wait():
+#     """Make the customer service wait"""
+#     response = VoiceResponse()
+#     response.say("Thank you for picking up. I am an assistant and will connect you shortly")
+#     return str(response)
+
+
+# def send_alert(message):
+#     """Send a text alert"""
+#     twilio_client.messages.create(
+#         body=message,
+#         from_=TWILIO_NUMBER,
+#         to=TARGET_NUMBER
+#     )
+
+# @app.route('/recording_callback', methods=['POST'])
+# def recording_callback():
+#     """Receive recording status and download the recording"""
+#     recording_url = request.form.get('RecordingUrl')
+#     recording_sid = request.form.get('RecordingSid')
+
+#     if recording_url:
+#         logger.info(f"Recording URL received: {recording_url}")
+
+#         # Construct the local file name
+#         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+#         local_filename = os.path.join("/recordings", f"{timestamp}_{recording_sid}.wav")
+
+#         # Download the recording
+#         response = requests.get(recording_url + '.wav')
+#         if response.status_code == 200:
+#             with open(local_filename, 'wb') as f:
+#                 f.write(response.content)
+#             logger.info(f"Recording saved locally as: {local_filename}")
+#         else:
+#             logger.error(f"Failed to download recording: {response.status_code}")
+
+#     return '', 200
 
 @app.route("/test", methods=['GET'])
 def test():
@@ -140,6 +176,7 @@ def main():
     logger.info(f"FLASK_ADDRESS: {FLASK_ADDRESS}")
     # Start the Flask app
     app.run(debug=True, port=3000)
+
 
 if __name__ == "__main__":
     main()
